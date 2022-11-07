4bbb0bb70648df7420426ad575c6a09a08025fd4
TaeWooKim <dhkdtpdn@gmail.com>
2022-11-07 19:44:41 +0900

    김태우 로그인체크, 검색

---
+++ src/main/java/kr/or/team4/service/search.java
@@ -0,0 +1,27 @@
+package kr.or.team4.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.team4.action.Action;
+import kr.or.team4.action.ActionForward;
+import kr.or.team4.dao.MemberDao;
+
+public class search implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+
+		ActionForward forward = new ActionForward();
+		
+        MemberDao dao = new MemberDao();
+        
+        request.setAttribute("list", dao.getMemberDtoByLikeEmail(request.getParameter("search")));
+        
+        forward.setPath("/WEB-INF/test/test.jsp");
+        forward.setRedirect(false);
+        
+		return forward;
+	}
+
+}
