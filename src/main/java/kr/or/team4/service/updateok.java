171b68939fc6d9c4017f2933c059862b95e02b70
DOMAYA <c2hila@gmail.com>
2022-11-07 19:32:15 +0900

    update 수정

--- src/main/java/kr/or/team4/service/updateok.java
+++ src/main/java/kr/or/team4/service/updateok.java
@@ -9,23 +9,6 @@ import kr.or.team4.dao.MemberDao;
 import kr.or.team4.dto.MemberDto;
 
 
-/*
- * 	private boolean isRedirect = false; //뷰의 전환 여부 redirect or forward
-	private String path = null;	//이동 경로
-	public boolean isRedirect() {
-		return isRedirect;
-	}
-	public void setRedirect(boolean isRedirect) {
-		this.isRedirect = isRedirect;
-	}
-	public String getPath() {
-		return path;
-	}
-	public void setPath(String path) {
-		this.path = path;
-	}
- */
-
 public class updateok implements Action {
 
 	@Override
